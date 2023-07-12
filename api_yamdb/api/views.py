9cd9827ffd3e2764717747373ef86e05a795f7be
Евгений Александров <kenobio76@gmail.com>
2023-07-12 12:43:35 +0300

    added rating calculation to TitleViewSet

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.db.models import Avg
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
@@ -18,3 +19,25 @@ class CommentViewSet(viewsets.ModelViewSet):
         review_id = self.kwargs['review_id']
         review = get_object_or_404(Review, title_id=title, id=review_id)
         serializer.save(review=review, author=self.request.user)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Получаем/создаем/удаляем/редактируем произведение."""
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminOrReadOnly)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TitleFilter
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return TitleGetSerializer
+        return TitleSerializer
+
+    def get_queryset(self):
+        if self.action in ('list', 'retrieve'):
+            queryset = (Title.objects.prefetch_related('reviews').all().
+                        annotate(rating=Avg('reviews__score')).
+                        order_by('name'))
+            return queryset
+        return Title.objects.all()
